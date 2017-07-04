8631b220a0214d67f49ac6b90e348bc2ef69180c
FND <fnd@localhost.localdomain>
2017-07-04 11:57:08 +0200

    ensured children are flattened before being passed to macros
    this ensures predictable arguments for macro implementations:
    
        <list-group>
            {["foo", "bar", "baz"].map(item => {
                return <span>{item}</span>;
            })}
        </list-group>
    
        registerMacro("list-group", (_, ...items) => {
            return <ul>
                {items.map(item => {
                    return <li>{item}</li>;
                })}
            </ul>;
        });
    
    otherwise the list-group macro would have to flatten `items` itself

--- src/renderer.js
+++ src/renderer.js
@@ -1,4 +1,5 @@
 import generateHTML from "./html";
+import { flatCompact } from "./util";
 
 const TAG_MACROS = {};
 
@@ -19,5 +20,6 @@ export function registerMacro(tag, fn) { // TODO: rename?
 
 export function createElement(tag, params, ...children) {
 	let macro = TAG_MACROS[tag];
-	return macro ? macro(params, ...children) : generateHTML(tag, params, ...children);
+	return macro ? macro(params, ...flatCompact(children)) :
+			generateHTML(tag, params, ...children);
 }
