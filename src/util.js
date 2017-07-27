7120a186068a16a247cbaccfff7d4ee96181e10f
FND <fnd@localhost.localdomain>
2017-07-27 09:58:11 +0200

    improved backwards compatibility by avoiding `Array#includes`
    not all JS engines implement this

--- src/util.js
+++ src/util.js
@@ -3,7 +3,7 @@ const BLANKS = [undefined, null, false];
 // flatten array while discarding blank values
 export function flatCompact(items) {
 	return items.reduce((memo, item) => {
-		return BLANKS.includes(item) ? memo :
+		return BLANKS.indexOf(item) !== -1 ? memo :
 				memo.concat(item.pop ? flatCompact(item) : item);
 	}, []);
 }
