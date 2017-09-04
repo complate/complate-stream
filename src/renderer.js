78d71adffa485d90cae500f257f7259f5c6884de
FND <fnd@localhost.localdomain>
2017-09-04 18:25:58 +0200

    restored macro registry for views' entry points
    host environments typically don't have access to the respective macro
    function (cf. 04b8eda2c18c8b1ff0c5d1a7647f323763ec2dfb) - so for views'
    entry points, we need a way to identify macros via strings after all
    
    in the process, added support for HTML fragments (as opposed to
    documents; fragments omit the doctype and typically also omit layout,
    though the latter is up to the respective entry-point macro)

--- src/renderer.js
+++ src/renderer.js
@@ -1,12 +1,49 @@
 import generateHTML from "./html";
 import { flatCompact, noop } from "./util";
 
-export default function documentRenderer(doctype = "<!DOCTYPE html>") {
-	return (stream, tag, params, callback) => {
-		if(doctype) {
+// generates a pair of functions:
+// `render` serves as the API for the host environment
+// `registerView` allows `render` to reference registered macros by their name
+export default function renderer(doctype = "<!DOCTYPE html>") {
+	let macros = {};
+
+	let registerView = (macro, name = macro.name) => {
+		if(!name) {
+			throw new Error(`missing name for macro: \`${macro}\``);
+		}
+
+		if(macros[name]) {
+			throw new Error(`invalid macro name: \`${name}\` already registered`);
+		}
+		macros[name] = macro;
+
+		return name; // primarily for debugging
+	};
+
+	// `view` is either a macro function or a string identifying a registered macro
+	// `params` is a mutable key-value object which is passed to the respective macro
+	// `stream` is a writable stream (cf. `generateHTML`)
+	// `fragment` is a boolean determining whether to omit doctype and layout
+	// `callback` is an optional function invoked upon conclusion - if provided,
+	// this activates non-blocking rendering
+	let render = (view, params, stream, fragment, callback) => {
+		if(!fragment) {
 			stream.writeln(doctype);
 		}
-		let element = createElement(tag, params);
+
+		if(fragment) {
+			if(!params) {
+				params = {};
+			}
+			params._layout = false; // XXX: hacky? (e.g. might break due to immutability)
+		}
+
+		// resolve strings to corresponding macro
+		let macro = (view && view.substr) ? macros[view] : view;
+		if(!macro) {
+			throw new Error(`unknown macro: \`${view}\``);
+		}
+		let element = createElement(macro, params);
 
 		if(callback) { // non-blocking mode
 			element(stream, true, callback);
@@ -14,6 +51,8 @@ export default function documentRenderer(doctype = "<!DOCTYPE html>") {
 			element(stream, false, noop);
 		}
 	};
+
+	return { render, registerView };
 }
 
 // distinguishes regular tags from macros
--- test/test_renderer.js
+++ test/test_renderer.js
@@ -1,82 +1,94 @@
 /* global describe, it */
 import { SiteIndex, BlockingContainer, NonBlockingContainer } from "./macros";
 import WritableStream from "./stream";
-import renderer from "../src/renderer";
+import renderer, { createElement } from "../src/renderer";
 import assert from "assert";
 
 describe("renderer", _ => {
 	it("should generate a render function for streaming HTML documents", done => {
-		let stream = new WritableStream();
-		let render = renderer(); // defaults to HTML5
+		let { render, stream } = setup(); // renderer defaults to HTML5
 
-		render(stream, "html", null, _ => {
+		render(HTMLRoot, null, stream, false, _ => {
 			assert.equal(stream.read(), "<!DOCTYPE html>\n<html></html>");
 			done();
 		});
 	});
 
 	it("should support custom doctypes", done => {
-		let stream = new WritableStream();
-		let render = renderer("<!DOCTYPE … XHTML …>");
+		let { render, stream } = setup("<!DOCTYPE … XHTML …>");
 
-		render(stream, "html", null, _ => {
+		render(HTMLRoot, null, stream, false, _ => {
 			assert.equal(stream.read(), "<!DOCTYPE … XHTML …>\n<html></html>");
 			done();
 		});
 	});
 
-	it("should support omitting doctype for HTML fragments", done => {
-		let stream = new WritableStream();
-		let render = renderer(null);
+	it("should omit doctype for HTML fragments", done => {
+		let { render, stream } = setup();
 
-		render(stream, "html", null, _ => {
+		render(HTMLRoot, null, stream, true, _ => {
 			assert.equal(stream.read(), "<html></html>");
 			done();
 		});
 	});
 
 	it("should support blocking mode", done => {
-		let stream = new WritableStream();
-		let render = renderer(null);
+		let { render, stream } = setup();
 
-		render(stream, BlockingContainer, null);
+		render(BlockingContainer, null, stream, true);
 		assert.equal(stream.read(),
 				"<div><p>…</p><p><i>lorem<em>…</em>ipsum</i></p><p>…</p></div>");
 		done();
 	});
 
 	it("should detect non-blocking child elements in blocking mode", done => {
-		let stream = new WritableStream();
-		let render = renderer(null);
+		let { render, stream } = setup();
 
-		let fn = _ => render(stream, NonBlockingContainer, null);
+		let fn = _ => render(NonBlockingContainer, null, stream);
 		assert.throws(fn, /invalid non-blocking operation/);
 		done();
 	});
 
-	it("should render unknown elements", done => {
-		renderFragment("custom-element", null, html => {
-			assert.equal(html, "<custom-element></custom-element>");
-			done();
-		});
-	});
-
 	it("should perform markup expansion for macros", done => {
-		renderFragment(SiteIndex, { title: "hello world" }, html => {
-			assert.equal(html, "<html>" +
+		let { render, stream } = setup();
+
+		render(SiteIndex, { title: "hello world" }, stream, true, _ => {
+			assert.equal(stream.read(), "<html>" +
 					'<head><meta charset="utf-8"><title>hello world</title></head>' +
 					"<body><h1>hello world</h1><p>…</p></body>" +
 					"</html>");
 			done();
 		});
 	});
+
+	it("should resolve registered macros", done => {
+		let { render, registerView, stream } = setup();
+
+		registerView(SiteIndex);
+		render("SiteIndex", { title: "hello world" }, stream, true, _ => {
+			assert.equal(stream.read(), "<html>" +
+					'<head><meta charset="utf-8"><title>hello world</title></head>' +
+					"<body><h1>hello world</h1><p>…</p></body>" +
+					"</html>");
+			done();
+		});
+	});
+
+	it("should balk at unregistered macros", done => {
+		let { render, stream } = setup();
+
+		let fn = _ => render("foo", null, stream);
+		assert.throws(fn, /unknown macro/);
+		done();
+	});
 });
 
-function renderFragment(tag, params, callback) {
-	let render = renderer(null);
+function HTMLRoot() {
+	return createElement("html");
+}
+
+function setup(doctype) {
 	let stream = new WritableStream();
-	render(stream, tag, params, _ => {
-		let html = stream.read();
-		callback(html);
-	});
+	let { render, registerView } = renderer(doctype);
+	return { render, registerView, stream };
 }
