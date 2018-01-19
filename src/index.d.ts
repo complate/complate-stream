d700bd48f0d8cfaba2d13d8afff5b389721e4b8f
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2018-01-19 17:40:36 +0100

    Fix ident

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,48 +1,48 @@
 declare type elementGenerator = (
-  stream: Renderer.Stream,
-  nonBlocking: boolean,
-  callback: () => void
+	stream: Renderer.Stream,
+	nonBlocking: boolean,
+	callback: () => void
 ) => void;
 
 interface StatelessFunctionalComponent<P> {
-  (props: P): elementGenerator;
+	(props: P): elementGenerator;
 }
 
 declare function createElement<T>(
-  element: string | StatelessFunctionalComponent<T>,
-  params: T,
-  ...children
+	element: string | StatelessFunctionalComponent<T>,
+	params: T,
+	...children
 ): elementGenerator;
 
 declare function safe(str: string): Renderer.HTMLString;
 
 declare class Renderer {
-  constructor(doctype?: string);
+	constructor(doctype?: string);
 
-  renderView(
-    view: elementGenerator | string,
-    params: Object,
-    stream: Renderer.Stream,
-    { fragment: boolean }?,
-    callback?: () => void
-  ): void;
+	renderView(
+		view: elementGenerator | string,
+		params: Object,
+		stream: Renderer.Stream,
+		{ fragment: boolean }?,
+		callback?: () => void
+	): void;
 
-  registerView(
-    macro: () => elementGenerator,
-    name?: string,
-    replace?: boolean
-  ): string;
+	registerView(
+		macro: () => elementGenerator,
+		name?: string,
+		replace?: boolean
+	): string;
 }
 
 declare namespace Renderer {
-  export interface Stream {
-    write(msg: string): void;
-    writeln(msg: string): void;
-    flush(): void;
-  }
-  export class HTMLString {
+	export interface Stream {
+		write(msg: string): void;
+		writeln(msg: string): void;
+		flush(): void;
+	}
+	export class HTMLString {
 
-  }
+	}
 }
 
 export default Renderer;
@@ -51,8 +51,8 @@ export { createElement, safe };
 
 // Global definitions
 declare global {
-  namespace JSX {
-    // This is needed for attribute type checking in custom components
-    interface Element extends StatelessFunctionalComponent<any> { }
-  }
+	namespace JSX {
+		// This is needed for attribute type checking in custom components
+		interface Element extends StatelessFunctionalComponent<any> { }
+	}
 }
