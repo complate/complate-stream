29f04b2b4d755437a9e1c540775c289d71c2f270
FND <fnd@localhost.localdomain>
2018-02-19 13:47:40 +0100

    upgraded dependencies

--- test/nashorn/faucet.config.js
+++ test/nashorn/faucet.config.js
@@ -5,9 +5,7 @@ module.exports = {
 		source: "./index.js",
 		target: "./dist/bundle.js",
 		moduleName: "render",
-		transpiler: {
-			features: ["es2015", "jsx"],
-			jsx: { pragma: "createElement" }
-		}
+		esnext: true,
+		jsx: { pragma: "createElement" }
 	}]
 };
