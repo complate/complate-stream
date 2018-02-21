944f45c3b265f7101b19256ca6740c88966008ba
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2018-02-21 21:54:45 +0100

    added optional TypeScript type definitions
    this enables TypeScript users to
    
    * use typing in TSX
    * `import … from "complate-stream"` (TypeScript doesn't support
      `package.json`'s `main` field)

---
+++ src/index.d.ts
@@ -0,0 +1,74 @@
+export default Renderer;
+export { createElement, generateHTML, safe, htmlEncode };
+
+// ** TSX configuration
+
+declare global {
+	namespace JSX {
+		// This is needed for attribute type checking in custom components
+		interface Element extends StatelessFunctionalComponent<any> { }
+	}
+}
+
+// ** Types and interfaces
+
+// Return value of createElement
+declare type elementGenerator = (
+	stream: Renderer.Stream,
+	nonBlocking: boolean,
+	callback: () => void
+) => void;
+
+// A macro
+interface StatelessFunctionalComponent<T> {
+	(props: T): elementGenerator;
+}
+
+// ** Exports
+
+declare const Fragment;
+
+declare function createElement<T>(
+	element: string | StatelessFunctionalComponent<T>,
+	params: T,
+	...children
+): elementGenerator;
+
+declare function generateHTML(
+	tag: string,
+	params,
+	...children
+): elementGenerator;
+
+declare function safe(str: string): Renderer.HTMLString;
+
+declare function htmlEncode(str: string, attribute: boolean): string;
+
+declare class Renderer {
+	constructor(doctype?: string);
+
+	renderView(
+		view: elementGenerator | string,
+		params: Object,
+		stream: Renderer.Stream,
+		{ fragment: boolean }?,
+		callback?: () => void
+	): void;
+
+	registerView(
+		macro: () => elementGenerator,
+		name?: string,
+		replace?: boolean
+	): string;
+}
+
+declare namespace Renderer {
+	export interface Stream {
+		write(msg: string): void;
+		writeln(msg: string): void;
+		flush(): void;
+	}
+	export class HTMLString {
+
+	}
+}
